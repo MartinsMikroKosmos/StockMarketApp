a7ea600d82561702a0ed346e8d655bcd95f68281
mrichter <m.richter@hh-berlin.de>
2024-02-06 14:36:05 +0100

    Created: ViewModel CSVParser CompanyListingsEvent CompanyListingsParser CompanyListingState StockRepository StockRepositoryImpl
    Changed:
    StockApi

---
+++ app/src/main/java/com/example/stockmarketapp/presentation/companyListings/CompanyListingsEvent.kt
@@ -0,0 +1,7 @@
+package com.example.stockmarketapp.presentation.companyListings
+
+sealed class CompanyListingsEvent {
+    object Refresh : CompanyListingsEvent()
+
+    data class OnSearchQueryChange(val query: String) : CompanyListingsEvent()
+}
